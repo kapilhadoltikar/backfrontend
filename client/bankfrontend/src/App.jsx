3b290ad1b47fc50973cbd7690ad2ff509465dec8
kapilh <146179927+kapilhadoltikar@users.noreply.github.com>
2025-03-29 12:27:52 +0530

    scroll, navbar, spa etc

---
+++ client/bankfrontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+import Analytics from './components/Analytics'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Login from './components/Login'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+import Signin from './components/Signin'
+import User from './components/User'
+import Services from './components/Services'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      {/* <Hero />
+      <Login />
+      <Newsletter />
+      <Services />
+      <Signin />
+      <Footer /> */}
+      
+      {/* <User />
+      <Footer /> */}
+    </div>
+  )
+}
+
+export default App
